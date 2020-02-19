13fc1f8e63a9a40cf1fd19f58cda382b064247f3
Leider Correa <42642466+Rediel18@users.noreply.github.com>
2020-02-19 13:51:35 -0500

    Add files via upload

--- ww.py
+++ ww.py
@@ -1,16 +1,16 @@
 import pymysql.cursors
 
 Db = "Prueba"
-contraseña = "261120"
+contraseña = "enlacasa"
 op = 0
 
 while op != 8:
     print("\n")
     print("1. Crear base de datos")
     print("2. Crear tabla")
-    print("3. Instertar un registro")
+    print("3. Agregar estudiante")
     print("4. Listar tabla")
-    print("5. Listar una sola fila")
+    print("5. Listar un estudiante")
     print("6. Actualiza un registro")
     print("7. Retira un registro")
     print("8. Termina\n")
@@ -47,7 +47,7 @@ while op != 8:
         try:
             CrearTabla = """CREATE TABLE Estudiantes(
                             Nombre Char(30) NOT NULL,
-                            Codigo INT(12) NOT NULL,
+                            Codigo BIGINT(13) NOT NULL,
                             Nota1 FLOAT(4) DEFAULT NULL,
                             Nota2 FLOAT(4) DEFAULT NULL,
                             Nota3 FLOAT(4) DEFAULT NULL,
@@ -72,15 +72,25 @@ while op != 8:
 
         cursor = conexion.cursor()
 
-        Nombre = input("Digite nombre del estudiante: ")
-        Codigo = input("Digite codigo del estudiante: ")
-        Nota1 = input("Digite nota del Corte 1: ")
-        Nota2 = input("Digite nota del Corte 2: ")
-        Nota3 = input("Digite nota del Corte 3: ")
-        NotaFinal = float(Nota1) * 0.35 + float(Nota2) * 0.35 + float(Nota3) * 0.30
+        Codigo = int(input("Digite codigo del estudiante: "))
 
+        MostarTabla1 = "select * from Estudiantes\
+            where Codigo=%s"
+        cursor.execute(MostarTabla1, (Codigo, ))
+        cursor.fetchone()
+        if cursor.rowcount >= 1:
+            print("Ya existe el estudiante "+str(Codigo))
+            cursor.close()
+            conexion.close()
+        else:
+            Nombre = input("Digite nombre del estudiante: ")
+            Nota1 = input("Digite nota del Corte 1: ")
+            Nota2 = input("Digite nota del Corte 2: ")
+            Nota3 = input("Digite nota del Corte 3: ")
+        
+        NotaFinal = float(Nota1) * 0.35 + float(Nota2) * 0.35 + float(Nota3) * 0.30
         InsertarRegistro = """insert into Estudiantes(Nombre, Codigo, Nota1, Nota2, Nota3, NotaFinal) values
-         (%s,%s,%s,%s,%s,%s)"""
+        (%s,%s,%s,%s,%s,%s)"""
         cursor.execute(InsertarRegistro, (Nombre, Codigo, Nota1, Nota2, Nota3, NotaFinal))
         print("Registro Agregado <3")
         cursor.close()
@@ -94,52 +104,104 @@ while op != 8:
             database=Db
         )
 
-        MostrarTabla = """
-            SELECT * FROM Estudiantes;
-        """
+        try:
+            MostrarTabla = "select * from Estudiantes"
+            cursor = conexion.cursor()
+            cursor.execute(MostrarTabla)
+            record = cursor.fetchall()
+            print("---> Numero de filas: ", cursor.rowcount)
+            for fila in record:
+                print("{:<4} {:<4} {:<4} {:<4} {:<4} {:<4} ".format(fila[0], fila[1], 
+                fila[2], fila[3], fila[4], fila[5]))
+            cursor.close()
+            conexion.close()
 
+        except:
+            print("Error en conexion")
+    
+    elif op == 5:
+        print("Listar un estudiante")
+        conexion = pymysql.connect(
+            host="localhost",
+            user="root",
+            passwd=contraseña,
+            database=Db
+        )
+
+        try: 
+            MostarTabla1 = "select * from Estudiantes\
+                where  Codigo=%s"
+            cursor = conexion.cursor()
+            Codigo = int(input("Digite codigo del estudiante: "))
+
+            cursor.execute(MostarTabla1, (Codigo, ))
+            fila = cursor.fetchone()
+            print("---> Numero de filas: ", cursor.rowcount)
+            if cursor.rowcount == 0:
+                print ("No existe estudiante"+str(Codigo))
+            else:
+                print("Nombre = ", fila[0])
+                print("Codigo = ", fila[1])
+                print("Nota1= ", fila[2])
+                print("Nota2 = ", fila[3])
+                print("Nota3 = ", fila[4])
+                print("NotaFinal = ", fila[5])
+            cursor.close()
+            conexion.close()
+
+        except:
+           print("Error en conexion")  
+
+    elif op == 6:
+        print("Actualizar nota de un estudiante")
+        conexion = pymysql.connect(
+            host="localhost",
+            user="root",
+            passwd=contraseña,
+            database=Db
+        )
+
+        #try: 
+        MostarTabla1 = "select * from Estudiantes\
+            where  Codigo=%s"
         cursor = conexion.cursor()
-        cursor.execute(MostrarTabla)
-        aux = cursor.fetchall()
-        print("\nNúmero de filas:  ", cursor.rowcount)
+        Codigo = int(input("Digite codigo del estudiante: "))
+        cursor.execute(MostarTabla1, (Codigo, ))
+        fila = cursor.fetchone()
+        print("---> Numero de filas: ", cursor.rowcount)
+        if cursor.rowcount == 0:
+            print ("No existe estudiante"+str(Codigo))
+        else:
+            Nota1 = int(input("Digite Nota1 del estudiante: "))
+            Nota2 = int(input("Digite Nota2 del estudiante: "))
+            Nota3 = int(input("Digite Nota3 del estudiante: "))
 
-        for fila in cursor:
-            print(fila)
-        #for i in gg:
-        #    print(gg[i])
-        cursor.close()
-        conexion.close()
+            #actualiza = "update Estudiantes set Nota1 =%s where Codigo = %s"
+            """actualiza = "update Estudiantes set Nota2 =%s where Codigo = %s"
+            actualiza = "update Estudiantes set Nota3 =%s where Codigo = %s"
+            cursor.execute(actualiza, (Nota1, Codigo))
+            cursor.execute(actualiza, (Nota2, Codigo))
+            cursor.execute(actualiza, (Nota2, Codigo))"""
 
+            sql = "UPDATE Estudiantes SET Nota1 = %s WHERE Nota1 = %s"
+            val = ("Nota1,")
 
+            cursor.execute("SELECT COUNT(*) FROM Estudiantes WHERE Codigo IN(Nota1, Nota2, Nota3)")
+            cursor.execute("UPDATE Estudiantes SET Value=0 WHERE Codigo IN(Nota1, Nota2, Nota3)")
+            
+            conexion.commit()
+            NotaFinal = float(Nota1) * 0.35 + float(Nota2) * 0.35 + float(Nota3) * 0.30
+            #InsertarRegistro = """insert into Estudiantes(Nombre, Codigo, Nota1, Nota2, Nota3, NotaFinal) values
+            #(%s,%s,%s,%s,%s,%s)"""
 
+            
+            print("Notas actualizadas")
+            cursor.close()
+            conexion.close()
+        #except:
+         #   print("Error durante el proceso")
 
-
-"""try:
-    cursor
-    print("\nSe Pudo Conectar con la DB")
-except:
-    print("\nNo Se Pudo Conectar con la DB")
-try:
-    cursor.execute(tabla)
-    print("\nSe acaba de crear la DB")
-except:
-    print("\n Ya estaba creada la DB")
-
-Nombre = input("Digite nombre del estudiante: ")
-Codigo = input("Digite codigo del estudiante: ")
-Nota1 = input("Digite nota del Corte 1: ")
-Nota2 = input("Digite nota del Corte 2: ")
-Nota3 = input("Digite nota del Corte 3: ")
-NotaFinal = float(Nota1)*0.35 + float(Nota2)*0.35 + float(Nota3)*0.30
-
-print(Nombre)
-print(NotaFinal)
-
-InsertarRegistro = "insert into Estudiantes(Nombre, Codigo, Nota1, Nota2, Nota3, NotaFinal) values (%s,%s,%s,%s,%s,%s)"
-cursor.execute(InsertarRegistro, (Nombre, Codigo, Nota1, Nota2, Nota3, NotaFinal))
-cursor.close()
-conexion.commit()
-
+"""
 otro
 cursor1.execute("select codigo, descripcion, precio from articulos")
 for fila in cursor1:
